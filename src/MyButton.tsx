7f3b1eac68f40483af5f297ea78f3ec663bfff53
MuhammdSheraz <mshazy999@gmail.com>
2021-01-06 22:44:59 +0500

    workings with addon added

--- src/MyButton.tsx
+++ src/MyButton.tsx
@@ -1,15 +1,16 @@
 import React from "react";
-
 const MyButton = ({
   value,
   style,
   onclick,
   className,
+  customColor,
 }: {
   value: string;
   style: {};
   className: string;
-  onclick: ()=>void;
+  customColor: string;
+  onclick: () => void;
 }) => {
   return (
     <button className={className} style={style} onClick={onclick}>
--- src/stories/MyButton.stories.tsx
+++ src/stories/MyButton.stories.tsx
@@ -1,24 +1,38 @@
 import MyButton from "../MyButton";
+import { withKnobs, text, object } from "@storybook/addon-knobs";
 
 export default {
   title: "MyButton",
   component: MyButton,
+  decorators: [withKnobs],
 };
+const styleList = {
+  padding: "15px",
+  backgroundColor: "gray",
+  color: "white",
+  fontSize: "22px",
+  borderRadius: "30px",
+  outline: "none",
+};
+// export const myButtomsm = () => {
+//   return (
+//     <MyButton
+//       customColor="orange"
+//       value="hello"
+//       className="myButton"
 
-export const myButtomsm = () => {
+//       onclick={() => alert("Hello I am Button")}
+//     />
+//   );
+// };
+export const addonButton = () => {
   return (
     <MyButton
-      value="hello"
+      customColor={text("Yellow", "Add Custom color")}
+      value={text("AddonButton", "Label")}
       className="myButton"
-      style={{
-        padding: "15px",
-        backgroundColor: "black",
-        color: "white",
-        fontSize: "22px",
-        borderRadius: "30px",
-        outline:"none"
-      }}
-      onclick={()=>alert("Hello I am Button")}
+      style={object("data", styleList)}
+      onclick={() => alert("Hello I am Button")}
     />
   );
 };
